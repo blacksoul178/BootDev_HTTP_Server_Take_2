7b51c82b7f102954325156ff24453b9d2fa7b7e8
Marc-André <blacksoul178@gmail.com>
2026-01-17 18:30:07 -0500

    added logger and external config reusable code for my future projects also step 5 of the course

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"HTTP_Server_2/internal/config"
+	"HTTP_Server_2/internal/logger"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// 1- Load app configs
+	appConfig, err := config.LoadAppConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading application configuration: %v", err)
+	}
+
+	// 2- Init logger
+	err = logger.InitLogger(appConfig)
+	if err != nil {
+		log.Fatalf("Failed to initialize logger: %v", err)
+	}
+
 	const port = "8080"
+	const filepathRoot = "."
 
 	mux := http.NewServeMux()
 
@@ -14,6 +29,8 @@ func main() {
 		Addr:    ":" + port,
 		Handler: mux,
 	}
+
+	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(srv.ListenAndServe())
